ad74c9844a554bb601b6f444580ed18f2a923d8a
AlexZhangiscool <99517968+AlexZhangiscool@users.noreply.github.com>
2022-02-13 10:06:55 -0500

    Update research bot 3000.py

--- research bot 3000.py
+++ research bot 3000.py
@@ -8,20 +8,28 @@ sleep(5)
 list_links = driver.find_elements_by_tag_name('a')
 repeats = []
 data = []
+loopy = []
 for i in list_links:
-        link = str(i.get_attribute('href'))
-        print(i.get_attribute('href'))
+        loopy.append(i.get_attribute('href'))
+
+for i in loopy:
+        link = str(i)
+        print(i)
         if link in repeats:
                 print("goober dectected, skip engaged")
                 continue
         elif link.count("2022") >= 1:
                 repeats.append(link)
-                i.click()
+                driver.get(i)
                 sleep(5)
                 text = driver.find_element_by_xpath("/html/body").text
                 data.append(text.count(keyword))
                 driver.back()
+                sleep(2)
+                print(data)
 
 
+print(data)
 sleep(3000)
 driver.quit()
+
