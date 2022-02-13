5ed0d74a9957f197c6aee16aabc6f3390c343b9f
AnonymousMystic <99548669+AnonymousMystic@users.noreply.github.com>
2022-02-13 14:38:27 -0500

    Update bollinger_band_code_+_other_possible_formulas.py

--- bollinger_band_code_+_other_possible_formulas.py
+++ bollinger_band_code_+_other_possible_formulas.py
@@ -11,6 +11,29 @@ import pandas as pd
 import numpy as np
 import math
 
+#//CODE I FOUND ONLINE
+import numpy as np
+import pandas as pd
+import pandas_datareader as pdr
+import matplotlib.pyplot as plt
+
+def get_sma(prices, rate):
+    return prices.rolling(rate).mean()
+
+def get_bollinger_bands(prices, rate=20):
+    sma = get_sma(prices, rate)
+    std = prices.rolling(rate).std()
+    bollinger_up = sma + std * 2 # Calculate top band
+    bollinger_down = sma - std * 2 # Calculate bottom band
+    return bollinger_up, bollinger_down
+
+symbol = 'AAPL'
+df = pdr.DataReader(symbol, 'yahoo', '2014-07-01', '2015-07-01')
+df.index = np.arange(df.shape[0])
+closing_prices = df['Close']
+
+bollinger_up, bollinger_down = get_bollinger_bands(closing_prices)
+
 #<<THIS WILL BE BASED ON THE ORGANIZED DATA FROM PANDAS>>
 #<<ALSO PLZ HELP WITH MAKING THIS A CONSTANTLY UPDATING CODE
 
