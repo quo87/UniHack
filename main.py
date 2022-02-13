a930056d16cc3145e33583779ddf879ef1ebbe67
Jax Kai <Zynxia38@outlook.com>
2022-02-13 14:51:06 -0500

    main

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import interface
+import research
 import data_organizer
 
 interface.startGUI()
 print(interface.word)
+research.extract(interface.word)
+print(research.data)
+data_organizer.list_to_series(research.data)
